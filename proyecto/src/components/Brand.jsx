bf3efeeae1aba1a2074149d94fab841a8c737d90
abelvera <abelvera9@gmail.com>
2023-10-16 23:12:58 -0300

    proyecto

--- proyecto/src/components/Brand.jsx
+++ proyecto/src/components/Brand.jsx
@@ -1,8 +1,15 @@
+import React from 'react'
+import { Link } from "react-router-dom"
 
 function Brand() {
   return (
-    <header className="text-3xl font-bold underline">Shop-Online!!!</header>
+    <div>
+       
+       <li  class="mr-3"><Link to="/" class="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" >brand</Link></li>
+
+    </div>
   )
 }
 
 export default Brand
+
--- proyecto/src/components/NavBar.jsx
+++ proyecto/src/components/NavBar.jsx
@@ -1,17 +1,27 @@
-import CartWidget from "./CartWidget"
+import { BrowserRouter, Route } from "react-router-dom";
+import CartWidget from "./CartWidget/CartWidget"
+import { Link } from "react-router-dom"
+
 
 
 const Navegador = () => {
 return (
-    <header>
-    <ul>
-      <li><a href="">shop</a></li>
-      <li><a href="">productos</a></li>
-      <li><a href="">login</a></li>
+    <header >
+    <ul   class="flex items-center justify-between flex-wrap bg-teal-500 p-2">
+      
+      <li  class="mr-3"><Link to="/shop" class="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" >shop</Link></li>
+      <li   class="mr-3"><Link to="/productos" class="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" >productos</Link></li>
+      <li  class="mr-3"><Link to="/login" class="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" >login</Link></li>
       <CartWidget/>
       
     </ul>
+
+    
+
+
   </header>
+
+
 
 
 )
@@ -19,5 +29,19 @@ return (
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default Navegador
